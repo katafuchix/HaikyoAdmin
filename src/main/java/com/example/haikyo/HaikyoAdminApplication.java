2e8344352c4324bf1158de0de6390af1c4eb9a97
katafuchix <katafuchix@gmail.com>
2023-10-10 19:43:38 +0900

    first commit

---
+++ src/main/java/com/example/haikyo/HaikyoAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.haikyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaikyoAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaikyoAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/haikyo/HaikyoAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haikyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaikyoAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
