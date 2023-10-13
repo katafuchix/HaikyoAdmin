699cd3f642b8c85432ac4564953d4db9ec16342e
katafuchix <katafuchix@gmail.com>
2023-10-13 16:45:15 +0900

    update WebMvcConfigurationSupport. add css/js

--- src/main/java/com/example/haikyo/config/CustomWebMvcConfigurationSupport.java
+++ src/main/java/com/example/haikyo/config/CustomWebMvcConfigurationSupport.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.SortHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -29,5 +30,21 @@ public class CustomWebMvcConfigurationSupport extends WebMvcConfigurationSupport
 		resolver.setPageParameterName("page");
 		resolver.setSizeParameterName("size");
 		    argumentResolvers.add(resolver);
+	}
+	
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/" };
+
+    @Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!registry.hasMappingForPattern("/webjars/**")) {
+			registry.addResourceHandler("/webjars/**")
+					.addResourceLocations("classpath:/META-INF/resources/webjars/");
+		}
+		if (!registry.hasMappingForPattern("/**")) {
+			registry.addResourceHandler("/**")
+					.addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+		}
 	}
 }
